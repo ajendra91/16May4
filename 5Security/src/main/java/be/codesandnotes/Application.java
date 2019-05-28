72f82530d38eb0428430cf751c44c10347446e30
ajendra <ginusiscorp@gmail.com>
2019-05-28 10:19:13 +0530

    Add files via upload

---
+++ 5Security/src/main/java/be/codesandnotes/Application.java
@@ -0,0 +1,15 @@
+package be.codesandnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.cassandra.CassandraDataAutoConfiguration;
+
+// SpringBootApplication conveniently declares @Configuration, @EnableAutoConfiguration and @ComponentScan.
+//@SpringBootApplication(exclude = CassandraDataAutoConfiguration.class)
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ 6Security-4Jwt/src/main/java/be/codesandnotes/security/SecurityConfiguration.java
@@ -0,0 +1,57 @@
+package be.codesandnotes.security;
+
+import be.codesandnotes.users.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.annotation.Resource;
+import java.security.NoSuchAlgorithmException;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    static final long TOKEN_LIFETIME = 604_800_000;
+    static final String TOKEN_PREFIX = "Bearer ";
+    static final String TOKEN_SECRET = "ThisIsOurSecretKeyToSignOurTokens";
+
+    @Resource
+    private AuthenticationEntryPoint authenticationEntryPoint;
+
+    @Resource
+    private MyUserDetailsService myUserDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() throws NoSuchAlgorithmException {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(myUserDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.csrf().disable();
+
+        http.exceptionHandling().authenticationEntryPoint(authenticationEntryPoint);
+
+        http
+                .httpBasic().disable()
+                .formLogin().disable()
+                .logout().disable()
+                .addFilter(new TokenBasedAuthenticationFilter(authenticationManager()))
+                .addFilter(new TokenBasedAuthorizationFilter(authenticationManager()));
+
+        http.authorizeRequests()
+                .antMatchers("/rest/unsecure/**").permitAll()
+                .antMatchers("/**").authenticated();
+    }
+}
